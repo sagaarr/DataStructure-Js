0001983039775cf1c18005835c06fe7de18ebbfc
sagar chinchorkar <sagarsc17@gmail.com>
2019-02-01 19:36:45 +0530

    Reverse added

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -129,6 +129,30 @@ remove(index){
   return aheadVal
 
 }
+ reverse(){
+  var node = this.head;
+  this.head = this.tail;
+  this.tail = node;
+  var next;
+  var prev = null;
+  for(var i = 0; i < this.length; i++){
+    next = node.next;
+    node.next = prev;
+    prev = node;
+    node = next;
+  }
+  return this;
+}
+ print(){
+    var arr = [];
+    var current = this.head
+    while(current){
+        arr.push(current.val)
+        current = current.next
+    }
+    console.log(arr);
+}
+
 }
 
 
