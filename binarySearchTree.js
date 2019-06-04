07a11969b44330d0a3b4e2006ff0bff2b36125ac
sagar chinchorkar <sagarsc17@gmail.com>
2019-06-04 17:38:26 +0530

    insert0

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -10,5 +10,31 @@ class BST{
   constructor(){
     this.root = null;
   }
-  
+  insert(value){
+    let newNode = new Node(value);
+    if(!this.root){
+      this.root = newNode;
+      return this;
+    }
+      let current = this.root;
+      while(true){
+        if(value == current.value) return undefined;
+          if(value < current.value){
+              if(current.left === null){
+                  current.left = newNode;
+                  return this;
+              }
+              current = current.left;
+          } else {
+              if(current.right === null){
+                  current.right = newNode;
+                  return this;
+              } 
+              current = current.right;
+          }
+      }
+      
+  }
 }
+
+let tree = new BST();
