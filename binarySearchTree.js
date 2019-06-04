9fdfb8f6edd8c26194bab3a44d4c07bc59a4b6a4
sagar chinchorkar <sagarsc17@gmail.com>
2019-06-04 20:58:05 +0530

    find added

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -35,6 +35,25 @@ class BST{
       }
       
   }
+
+  find(value){
+    if(!this.root) return false;
+    let current = this.root,
+        found = false;
+    while(current && !found){
+      // If value that user is trying to find is less than current value then move to the left side and change the current value to the current.left
+      if(value < current.value){
+        current = current.left;
+      }else if(value > current.value){
+         // If value that user is trying to find is greater than current value then move to the right side and change the current value to the current.right
+        current = current.right;
+      }else{
+        // this condition only be executed if "value" nither greater than or less than the current.value !!!
+        found = true;
+      }
+      return current;
+    }
+  }
 }
 
 let tree = new BST();
